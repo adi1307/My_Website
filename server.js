0a8880ce8641906eeba2711befe438a8f54bf527
adi1307 <aditya13kothari@gmail.com>
2020-03-02 00:34:46 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,27 @@
+/*let d, h, m, s, clock, color;
+
+function clocky(){
+    d = new Date();
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
+
+
+if( h <= 9){
+    h = '0' + h;
+}
+if( m <= 9){
+    m = '0' + m;
+}
+if( s <= 9){
+    s = '0' + s;
+}
+
+clock = h + ":" + m + ":" + s;
+color = "#"+ s + m + h;
+
+document.body.style.background = color;
+
+setTimeout(clocky, 1000);
+}
+clocky();*/
